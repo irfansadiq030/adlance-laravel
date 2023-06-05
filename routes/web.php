6588ec47f12993977c41d77e0d599cabbc253ab8
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2023-06-05 15:28:39 +0500

    Authentication Setup
    -- Login authentication setup with laravel breeze pkg.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+// Frontend Routes
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -52,3 +55,15 @@ Route::get('/terms-and-conditions', function () {
 Route::get('/career', function () {
     return view('career');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
