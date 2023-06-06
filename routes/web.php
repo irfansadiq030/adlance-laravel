55c0e07589ffce21d8ddc74eedab92ab57f0fc26
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2023-06-06 14:10:48 +0500

    Admin Dashboard Setup
    Added- Admin Dashboard Assets Added.
    Created Admin dashboard with breeze.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -55,6 +56,28 @@ Route::get('/terms-and-conditions', function () {
 Route::get('/career', function () {
     return view('career');
 });
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes Start
+|--------------------------------------------------------------------------
+*/
+
+Route::prefix('admin')->group(function () {
+
+    Route::get('/login', [AdminController::class, 'index'])->name('login_form');
+    Route::post('admin.login', [AdminController::class, 'admin_login'])->name('admin.login');
+    Route::get('dashboard', [AdminController::class, 'admin_dashboard'])->name('admin.dashboard')->middleware('admin');
+    Route::get('logout', [AdminController::class, 'admin_logout'])->name('admin.logout');
+    Route::get('register', [AdminController::class, 'register'])->name('admin.register');
+    Route::post('register', [AdminController::class, 'admin_register'])->name('admin.register.create');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes End
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/dashboard', function () {
     return view('dashboard');
